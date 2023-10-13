e07c29a036bef178f377c1f3a8f3ad628aca9f4a
Jessica E <122407139+jessicaeee@users.noreply.github.com>
2023-10-13 15:44:16 -0600

    Update httpclient.py

--- httpclient.py
+++ httpclient.py
@@ -41,13 +41,49 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        # search for equals in data
+        equal = re.match(r'^\S+\s+(\d+)', data)
+        # if no equal is found
+        if not equal:
+            return None
+        else: 
+            # if equal is found
+            if equal:
+                return int(equal.group(1))
+        
 
     def get_headers(self,data):
-        return None
+        # dictionary to keep track of headers
+        dictionary_header = {}
+        # create separators and split the data into parts
+        sep = '\r\n\r\n'
+        sep2 = '\r\n'
+        parts = data.split(sep, 1)[0]
+        # split the parts into lines
+        lines = parts.split(sep2)[1:]
+        # check every line and split them into parts
+        i = 0
+        while i < lines:
+            x, y = lines.split(':', 1)
+            dictionary_header[x] = y
+            i + 1
+        return dictionary_header
+
 
     def get_body(self, data):
-        return None
+        # separator between header and body and split into parts
+        sep = '\r\n\r\n'
+        index = data.rfind(sep)
+        part = data.split(sep, 1)
+        # if separator index is found
+        if index == -1:
+            return ''
+        else: 
+            # if list has more than one element
+            if len(part) > 1:
+                body = part[1]
+                return body
+
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,14 +104,114 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        # get info from url
+        url_analysis = urllib.parse.urlparse(url)
+        host = url_analysis.hostname
+
+        # get path from url
+        if url_analysis.path:
+            path = url_analysis.path
+        else: 
+            # if there is no path is available from url
+            if not url_analysis.path:
+                path = '/'
+
+        # get query from url
+        if url_analysis.query:
+            query = url_analysis.query
+        else: 
+            # if there is no query is available from url
+            if not url_analysis.query:
+                query = ''
+
+        # get port number from url
+        if url_analysis.port:
+            port = url_analysis.port
+        else: 
+            # if there is no port number is available from url
+            if not url_analysis.port:
+                port = 80
+
+        # form the questions to send
+        question = f"GET {path}?{query} HTTP/1.1\r\n"
+        question += f"Host: {host}\r\n\r\n"
+
+        # connect and send question to server
+        self.connect(host, port)
+        self.sendall(question)
+
+        # get answer from server
+        answer = self.recvall(self.socket)
+        code, body = (self.get_code(answer), self.get_body(answer))
+
+        # close connection to server
+        self.close()
         return HTTPResponse(code, body)
+        
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        # get info from url
+        url_analysis = urllib.parse.urlparse(url)
+        host = url_analysis.hostname
+
+        # get path from url
+        if url_analysis.path:
+            path = url_analysis.path
+        else: 
+            # if there is no path is available from url
+            if not url_analysis.path:
+                path = '/'
+        
+        # get query from url
+        if url_analysis.query:
+            query = url_analysis.query
+        else: 
+            # if there is no query is available from url
+            if not url_analysis.query:
+                query = ''
+        # get port number from url
+        if url_analysis.port:
+            port = url_analysis.port
+        else: 
+            # if there is no port number is available from url
+            if not url_analysis.port:
+                port = 80
+    
+        # if there is no argument
+        if not args:
+            posted = ''
+        else:
+            # get argument
+            if args:
+                posted = urllib.parse.urlencode(args)
+        # format POST
+        posting = "POST {}{} HTTP/1.1\r\n".format(path, query)
+        question = posting
+        # format host
+        hosting = "Host: {}\r\n".format(host)
+        # create question to send
+        question = question + hosting
+        # get content
+        content = "Content-Type: application/x-www-form-urlencoded\r\n"
+        question = question + content
+        # get the length of the content
+        content_length = "Content-Length: {}\r\n\r\n".format(len(posted))
+        question = question + content_length
+        # form completed question to send
+        question = question + posted
+        
+        # connect to server and send question
+        self.connect(host, port)
+        self.sendall(question)
+
+        # get answer from server
+        answer = self.recvall(self.socket)
+        code, body = (self.get_code(answer), self.get_body(answer))
+
+        # close connection to server
+        self.close()
         return HTTPResponse(code, body)
+        
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
@@ -93,3 +229,11 @@ if __name__ == "__main__":
         print(client.command( sys.argv[2], sys.argv[1] ))
     else:
         print(client.command( sys.argv[1] ))
+
+'''
+Sources Consulted:
+            https://note.nkmk.me/en/python-split-rsplit-splitlines-re/
+            https://stackoverflow.com/questions/6479423/does-d-in-regex-mean-a-digit
+            https://stackoverflow.com/questions/40557606/how-to-url-encode-in-python-3
+            https://www.youtube.com/watch?v=T19SWjvHrGk
+'''
